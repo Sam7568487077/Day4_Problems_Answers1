1ee81d7659a7fc31cbe77429dd679b606941d61b
Sam K Varghese <ksam071@gmail.com>
2023-01-02 21:16:15 +0530

    Solving using switch case

--- src/Employee_Wage_Computation.java
+++ src/Employee_Wage_Computation.java
@@ -1,26 +1,28 @@
+import java.util.Random;
 public class Employee_Wage_Computation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
-        int Is_Full_Time=1;
-        int Is_Half_Time=2;
-        int wage_per_hour=20;
-        int fullday=8;
-        int halfday=4;
-        double empcheck =Math.floor(Math.random()*10)%3;
-        if(Is_Full_Time == empcheck)
-        {
-            System.out.println(" Employee is present");
-            System.out.print(" Daily wage of employee is: " + wage_per_hour * fullday + "Rs");
+        int wage_per_hour = 20;
+        int fullday = 8;
+        int halfday = 4;
+        int overhead = 3;
+        Random sc = new Random();
+        int empcheck = sc.nextInt(overhead);
+
+        switch (empcheck) {
+            case 1 -> {
+                System.out.println(" Employee is present");
+                System.out.print(" Daily wage of employee is: " + wage_per_hour * fullday + "Rs");
+            }
+            case 2 -> {
+                System.out.println(" Employee is present but has worked half day");
+                System.out.print(" Daily wage of employee is: " + wage_per_hour * halfday + "Rs");
+            }
+            default -> {
+                System.out.println(" Employer is absent");
+                System.out.print(" Daily wage of employee is: 0 Rs");
+            }
         }
-        else if ( Is_Half_Time == empcheck)
-        {
-            System.out.println(" Employee is present but has worked half day");
-            System.out.print(" Daily wage of employee is: " + wage_per_hour * halfday + "Rs");
-        }
-        else
-        {
-            System.out.println(" Employer is absent");
-            System.out.print(" Daily wage of employee is: 0 Rs");
-        }
+
     }
 }
