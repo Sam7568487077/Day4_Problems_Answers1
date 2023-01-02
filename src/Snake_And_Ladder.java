108a868a0d428c87738c88b02dfaf8776603f7e9
Sam K Varghese <ksam071@gmail.com>
2023-01-02 18:58:08 +0530

    UC1 Current position set

---
+++ src/Snake_And_Ladder.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+public class Snake_And_Ladder {
+    public static void main(String[] args) {
+
+            int final_position=100; int initial_position=0;int count=0;
+            System.out.println(" === Welcome to snake and ladder =====");
+        System.out.println(" Your current position is " + initial_position );
+
+
+    }
+}
