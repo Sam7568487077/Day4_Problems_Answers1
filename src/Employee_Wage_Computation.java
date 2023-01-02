ff153176e2cb8b910b6b431a9772b7734abfe0ad
Sam K Varghese <ksam071@gmail.com>
2023-01-02 21:11:34 +0530

    UC 3 part time employee details

--- src/Employee_Wage_Computation.java
+++ src/Employee_Wage_Computation.java
@@ -2,13 +2,20 @@ public class Employee_Wage_Computation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
         int Is_Full_Time=1;
+        int Is_Half_Time=2;
         int wage_per_hour=20;
         int fullday=8;
-        double empcheck =Math.floor(Math.random()*10)%2;
+        int halfday=4;
+        double empcheck =Math.floor(Math.random()*10)%3;
         if(Is_Full_Time == empcheck)
         {
             System.out.println(" Employee is present");
             System.out.print(" Daily wage of employee is: " + wage_per_hour * fullday + "Rs");
+        }
+        else if ( Is_Half_Time == empcheck)
+        {
+            System.out.println(" Employee is present but has worked half day");
+            System.out.print(" Daily wage of employee is: " + wage_per_hour * halfday + "Rs");
         }
         else
         {
