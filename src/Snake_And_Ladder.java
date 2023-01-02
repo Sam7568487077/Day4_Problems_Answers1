78066d62efb49245f162e5d71c321123188a1312
Sam K Varghese <ksam071@gmail.com>
2023-01-02 19:01:38 +0530

    Input of Die Rolling

--- src/Snake_And_Ladder.java
+++ src/Snake_And_Ladder.java
@@ -6,6 +6,16 @@ public class Snake_And_Ladder {
             System.out.println(" === Welcome to snake and ladder =====");
         System.out.println(" Your current position is " + initial_position );
 
+        Random rm = new Random();
+        int dice_roll_value = rm.nextInt(1, 6);
+        System.out.print(" The dice value is : " +dice_roll_value);
+        System.out.println("\n");
+        System.out.print("You are on :" + initial_position);
+        System.out.println("\n");
+
+
+
+
 
     }
 }
