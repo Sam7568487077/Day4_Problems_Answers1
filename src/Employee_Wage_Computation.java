448f67a4c5cf31b8d55964f8b2eb8090b6a993f1
Sam K Varghese <ksam071@gmail.com>
2023-01-02 21:04:20 +0530

    TO check attendance of employees

--- src/Employee_Wage_Computation.java
+++ src/Employee_Wage_Computation.java
@@ -1,5 +1,15 @@
 public class Employee_Wage_Computation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
+        int Is_Full_Time=1;
+        double empcheck =Math.floor(Math.random()*10)%2;
+        if(Is_Full_Time == empcheck)
+        {
+            System.out.println(" Employee is present");
+        }
+        else
+        {
+            System.out.println(" Employer is absent");
+        }
     }
 }
