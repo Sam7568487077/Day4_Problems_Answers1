8411b04c13802842f6e1c318b8ffa321652b26d0
Sam K Varghese <ksam071@gmail.com>
2023-01-02 19:07:34 +0530

    Input of the Player chance

--- src/Snake_And_Ladder.java
+++ src/Snake_And_Ladder.java
@@ -7,11 +7,47 @@ public class Snake_And_Ladder {
         System.out.println(" Your current position is " + initial_position );
 
         Random rm = new Random();
+        Random rm2= new Random();
+
         int dice_roll_value = rm.nextInt(1, 6);
         System.out.print(" The dice value is : " +dice_roll_value);
         System.out.println("\n");
         System.out.print("You are on :" + initial_position);
         System.out.println("\n");
+
+        int board_items = rm2.nextInt(0, 3);
+        switch (board_items) {
+            case 0:
+                System.out.println("No Play");
+                System.out.println("\n");
+                initial_position = initial_position;
+                System.out.print("The dice rolled value is : " + dice_roll_value);
+                System.out.println("\n");
+                System.out.print(" Your current position is : " + initial_position);
+                System.out.println("\n");
+                break;
+            case 1:
+                System.out.println("Snake Present");
+                System.out.println("\n");
+                initial_position = initial_position-dice_roll_value;
+                System.out.print("The dice rolled value is : " + dice_roll_value);
+                System.out.println("\n");
+                System.out.print(" Your current position is : " + initial_position);
+                System.out.println("\n");
+                break;
+            case 3:
+                System.out.println("Ladder Present");
+                System.out.println("\n");
+                initial_position = initial_position+dice_roll_value;
+                System.out.print("The dice rolled value is : " + dice_roll_value);
+                System.out.println("\n");
+                System.out.print(" Your current position is : " + initial_position);
+                System.out.println("\n");
+                break;
+
+
+
+        }
 
 
 
